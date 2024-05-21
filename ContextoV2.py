a26d90df907b5d8919270f9847128261bc6421ab
EduardNeghina <neghina.eduard97@gmail.com>
2024-05-21 16:24:38 +0300

    WebController testing

--- ContextoV2.py
+++ ContextoV2.py
@@ -1,3 +1,9 @@
 from WebController import *
 
-driver = initiate_contexto()
+
+# driver = initiate_contexto()
+
+driver = WebController()
+driver.initiate_contexto()
+driver.click_3dots()
+time.sleep(3)
--- WebController.py
+++ WebController.py
@@ -6,10 +6,46 @@ import re
 import os
 
 
-def initiate_contexto():
-    driver = webdriver.Chrome()
-    driver.get('https://contexto.me/')
-    driver.maximize_window()
-    time.sleep(3)
-    driver.find_element(By.CLASS_NAME, 'fc-primary-button').click()
-    time.sleep(3)
+class WebController:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def initiate_contexto(self):
+        self.driver.get("https://contexto.me/")
+        self.driver.maximize_window()
+        time.sleep(3)
+        try:
+            self.driver.find_element(By.CLASS_NAME, 'fc-primary-button').click()
+        except:
+            print("Consent cookie button could not be pressed. initiate_contexto() failed")
+        else:
+            print("Contexto is fully initiated and ready to use")
+
+    def click_3dots(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, 'btn')
+        for e in elements:
+            e.click()
+            break
+
+    def click_previous_games(self):
+        self.__click_3dots()
+        elements = self._driver.find_elements(By.CLASS_NAME, 'menu-item')
+        for e in elements:
+            if e.text == 'Previous games':
+                e.click()
+                break
+
+    def __click_yes_for_give_up(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, 'share-btn')
+        for e in elements:
+            if e.text == 'Yes':
+                e.click()
+
+
+
+
+
+
+
+
+
