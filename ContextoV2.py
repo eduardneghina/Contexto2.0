b85fa65f064d6793d5265b9474ecaeff68c57c8e
EduardNeghina <neghina.eduard97@gmail.com>
2024-12-18 23:43:59 +0200

    refactoring to use logging + comments

--- ContextoV2.py
+++ ContextoV2.py
@@ -3,18 +3,9 @@ from WebController import *
 
 
 
-#############################################################
-########### reminder for future #########################################
-#############################################################
-# _driver = driver.initiate_contexto()
-# print(_driver)
-# output "string" - see initiate_contexto(self): finally
-#
-#
-#
-#
-#####################################################
-
 WebControllerObject = WebController()
 WebControllerObject.initiate_contexto()
 
+
+
+time.sleep(10)
--- WebController.py
+++ WebController.py
@@ -1,33 +1,28 @@
+import logging
+import time
+import re
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-#from random_word import RandomWords
-from wonderwords import RandomWord
-import time
-import re
-import os
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
-import random
+from webdriver_manager.chrome import ChromeDriverManager
+from wonderwords import RandomWord
 
-
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class WebController:
     def __init__(self):
         """
-
         Hardcoded to use Brave Browser
-
         """
-        #C:\Program Files\BraveSoftware\Brave-Browser\Application
-        BROWSER_PATH = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
+        browser_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
         options = Options()
-        options.binary_location = BROWSER_PATH
+        options.binary_location = browser_path
         service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=service, options=options)
-        #self.driver = webdriver.Chrome()
         self.db_path_directory = "C:\\Temp\\Contexto"
         self.db_path_file = "C:\\Temp\\Contexto\\data.txt"
 
@@ -37,46 +32,38 @@ class WebController:
         time.sleep(3)
         try:
             self.driver.find_element(By.CLASS_NAME, 'fc-primary-button').click()
-        except:
-            print("Consent cookie button could not be pressed. initiate_contexto() failed")
+        except Exception:
+            logging.error("Consent cookie button could not be pressed. initiate_contexto() failed")
         else:
-            print("Contexto is fully initiated and ready to use")
-        finally:
-            return "string?"
-
-##########################################################################
-#
-#   Private functions for different actions
-#
+            logging.info("Contexto is fully initiated and ready to use")
+        return "string?"
 
     def __click_yes_for_give_up(self):
         elements = self.driver.find_elements(By.CLASS_NAME, 'share-btn')
-        for e in elements:
-            if e.text == 'Yes':
-                e.send_keys(Keys.RETURN)
-                #e.click()
+        for element in elements:
+            if element.text == 'Yes':
+                element.send_keys(Keys.RETURN)
                 break
 
-    def insert_a_random_word_nouns_library(self):
-        r = RandomWord()
-        random_word = r.random_words(1, include_parts_of_speech=["nouns"])[0]
-        print("In random_word avem : " + random_word)
+    def __insert_random_word(self):
+        random_word = RandomWord().random_words(1, include_parts_of_speech=["nouns"])[0]
+        logging.info("Random word: %s", random_word)
         elements = self.driver.find_elements(By.CLASS_NAME, 'word')
-        for e in elements:
-            e.send_keys(random_word, Keys.ENTER)
+        for element in elements:
+            element.send_keys(random_word, Keys.ENTER)
             break
-        print("Cuvantul random introdus a fost " + random_word)
+        logging.info("Inserted random word: %s", random_word)
 
     def __load_data(self):
         try:
             with open(self.db_path_file, "r") as infile:
-                data = infile.read()
-            return data
+                return infile.read()
         except FileNotFoundError:
-            print("File/directory not found. We will try to create one")
+            logging.warning("File/directory not found. Creating new one.")
             os.makedirs(self.db_path_directory, exist_ok=True)
             with open(self.db_path_file, "w") as starting_file:
                 pass
+            return ""
 
     def __save_data(self, data):
         with open(self.db_path_file, "w") as outfile:
@@ -89,88 +76,18 @@ class WebController:
         elif not isinstance(new_data, str):
             new_data = str(new_data)
         if new_data in data:
-            print("Data is already in the file. No changes made.")
+            logging.info("Data is already in the file. No changes made.")
         else:
             data += "\n" + new_data
             self.__save_data(data)
-            print("Data added successfully.")
-#
-#   End of private functions
-#
-##########################################################################
-#
-# Start of basic functions like click on different buttons or simple actions like go to x game
-#
-    def click_3dots(self):
-        elements = self.driver.find_elements(By.CLASS_NAME, 'btn')
-        for e in elements:
-            e.click()
-            break
+            logging.info("Data added successfully.")
 
-    def click_give_up(self):
-        elements = self.driver.find_elements(By.CLASS_NAME, 'menu-item')
-        for e in elements:
-            if e.text == 'Give up':
-                e.click()
-                self.__click_yes_for_give_up()
-                break
+    def add_data(self, new_data):
+        data = self.__load_data()
+        data += "\n" + new_data
+        self.__save_data(data)
 
-    def click_closest_word(self):
-        elements = self.driver.find_elements(By.CLASS_NAME, 'button')
-        for e in elements:
-            if e.text == 'Closest words':
-                e.send_keys(Keys.RETURN)
-                #e.click() == The element must be visible on the page, ads or outscrolled elements can't be clicked. Use key return instead or Actions class
-                break
-
-    def click_previous_games(self):
-        elements = self.driver.find_elements(By.CLASS_NAME, 'menu-item')
-        for e in elements:
-            if e.text == 'Previous games':
-                e.click()
-                break
-    def click_desired_previous_games(self, game_number):
-        elements = self.driver.find_elements(By.CLASS_NAME, 'game-selection-button')
-        for e in elements:
-            if re.search('#' + str(game_number) + '\n', e.text) is not None:
-                self.driver.execute_script("arguments[0].scrollIntoView();", e)
-                e.click()
-                break
-    def click_on_random_from_previous_games(self):
-        elements = self.driver.find_elements(By.CLASS_NAME, 'button')
-        for e in elements:
-            if e.text == "Random":
-                e.click()
-                break
-
-    def get_game_number(self):
-        elements = self.driver.find_element(By.XPATH, "//span[contains(text(), '#')]")
-        gamenumber = elements.text.split('#')[1].strip()
-        #print(gamenumber)
-        return gamenumber
-
-    def click_on_x_to_close_previous_games(self):
-        elements = self.driver.find_elements(By.CLASS_NAME, "modal-close-button")
-        for e in elements:
-            e.click()
-            break
-#
-#   End of basic functions
-#
-##########################################################################
-#
-# Start of complex functions such as extracting data
-#
-
-    def extract_data_from_the_game(self):
-        """
-        The view must be in the desired game.
-        So : || click_desired_previous_games || should be used to be in the desired game number
-        Automatically : Give up + open "closest words" menu.
-        All the data is extracted and returned already parsed in form of :
-        [['chief', '1'], ['deputy', '2'], ['chairman', '3'], ['officer', '4'], ['vice', '5'] ... and so on
-        """
-
+    def extract_test(self):
         self.click_3dots()
         self.click_give_up()
         time.sleep(3)
@@ -178,32 +95,84 @@ class WebController:
         time.sleep(3)
         data = []
         elements = self.driver.find_elements(By.CLASS_NAME, 'row-wrapper')
-        for e in elements:
-            data.append(e.text.split('\n'))
-        res = []
-        [res.append(x) for x in data if x not in res]
-        return res
+        for element in elements:
+            data.append(element.text.split('\n'))
+        return list(dict.fromkeys(data))
 
+    def click_3dots(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, 'btn')
+        for element in elements:
+            element.click()
+            break
 
+    def click_give_up(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, 'menu-item')
+        for element in elements:
+            if element.text == 'Give up':
+                element.click()
+                self.__click_yes_for_give_up()
+                break
+
+    def click_closest_word(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, 'button')
+        for element in elements:
+            if element.text == 'Closest words':
+                element.send_keys(Keys.RETURN)
+                break
+
+    def click_previous_games(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, 'menu-item')
+        for element in elements:
+            if element.text == 'Previous games':
+                element.click()
+                break
+
+    def click_desired_previous_games(self, game_number):
+        elements = self.driver.find_elements(By.CLASS_NAME, 'game-selection-button')
+        for element in elements:
+            if re.search('#' + str(game_number) + '\n', element.text):
+                self.driver.execute_script("arguments[0].scrollIntoView();", element)
+                element.click()
+                break
+
+    def click_on_random_from_previous_games(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, 'button')
+        for element in elements:
+            if element.text == "Random":
+                element.click()
+                break
+
+    def get_game_number(self):
+        element = self.driver.find_element(By.XPATH, "//span[contains(text(), '#')]")
+        return element.text.split('#')[1].strip()
+
+    def click_on_x_to_close_previous_games(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, "modal-close-button")
+        for element in elements:
+            element.click()
+            break
+
+    def extract_data_from_game(self):
+        self.click_3dots()
+        self.click_give_up()
+        time.sleep(3)
+        self.click_closest_word()
+        time.sleep(3)
+        data = []
+        elements = self.driver.find_elements(By.CLASS_NAME, 'row-wrapper')
+        for element in elements:
+            data.append(element.text.split('\n'))
+        return list(dict.fromkeys(data))
 
     def extract_all_history_games(self):
-        """
-        Starts to fetch all games data and write the in a txt file, start with the most recent game and doesn't add doubles
-
-        """
-        for i in range(int(self.get_game_number())):
+        for _ in range(int(self.get_game_number())):
             time.sleep(1)
             try:
-                data = self.extract_data_from_the_game()
-                print(data)
-                print("urmeaza add data")
-                try:
-                    self.__add_data(data)
-                except Exception as f:
-                    print(f"An unexpected error occurred: {f}")
+                data = self.extract_data_from_game()
+                logging.info(data)
+                self.__add_data(data)
             except Exception as e:
-                print("something went wrong in extract_all_history_games ? ")
-                print(f"An unexpected error occurred: {e}")
+                logging.error("An unexpected error occurred: %s", e)
             else:
                 self.click_on_x_to_close_previous_games()
                 self.click_3dots()
@@ -213,4 +182,6 @@ class WebController:
                 time.sleep(1)
 
 
-# AI RAMAS LA 180
+#DE REPARAT SA SE OPREASCA CAND GASESTE UN CUVANT CA SA NU MEARGA LA INFINIT SI SA FIE UN UPDATE LOGIC + TO BE TESTED LEL
+
+#
